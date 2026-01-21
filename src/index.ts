43018d5342417f70e544fa502632de7c0af213bf
suraj-ufx <suraj@unifyxperts.com>
2026-01-21 14:08:54 +0530

    updated yml + index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,14 @@ import express, { type Request, type Response } from 'express'
 
 const app = express()
 
-app.get("/",(req:Request,res:Response) =>{
+app.get("/", (req: Request, res: Response) => {
     res.send("running dockerize service")
 })
 
-const PORT = 3000;
-app.listen(()=>console.log(`Server is running on ${PORT}`))
+const PORT = process.env.PORT || 3000;
+const HOST = '0.0.0.0';
+
+
+app.listen(3000, "0.0.0.0", () => {
+    console.log(`Server running at http://${HOST}:${PORT}`);
+})
